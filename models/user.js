3b02602e4d8e78ab52c15ec4db75574ad2011b44
Evil Cicada <x@urus.pw>
2025-08-25 21:36:19 +0100

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,1 +1,10 @@
 
+const mongoose = require("mongoose");
+
+const UserSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true, trim: true },
+  username: { type: String, required: true, unique: true, trim: true },
+  password: { type: String, required: true } // hashed
+}, { timestamps: true });
+
+module.exports = mongoose.model("User", UserSchema);
